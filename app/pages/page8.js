63f7330270543e6b3a89a13e602ead00662b0469
Luis Maia <luismaiasombra@gmail.com>
2026-01-21 09:45:54 -0300

    Texto Baixar .zip

--- app/pages/page8.js
+++ app/pages/page8.js
@@ -116,7 +116,7 @@ export function renderPage8() {
             <svg class="icon mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
             </svg>
-            Baixar Todos os Arquivos
+            Baixar .ZIP
           </button>
         </div>
       </div>
